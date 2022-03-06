e5490dfc5a395d26358b6d6af2fa6e3bd39ec55f
marcellalves <marcell.alves@gmail.com>
2022-03-06 19:07:55 -0300

    add new test and refactor variable names

--- src/test/groovy/shiftLinkedList/ShiftLinkedListTest.groovy
+++ src/test/groovy/shiftLinkedList/ShiftLinkedListTest.groovy
@@ -7,21 +7,21 @@ class LinkedListTest extends Specification {
         ShiftLinkedList shiftLinkedList = new ShiftLinkedList()
 
         given:
-        LinkedList shiftLinkedList0 = new LinkedList(0)
-        LinkedList shiftLinkedList1 = new LinkedList(1)
-        LinkedList shiftLinkedList2 = new LinkedList(2)
-        LinkedList shiftLinkedList3 = new LinkedList(3)
-        LinkedList shiftLinkedList4 = new LinkedList(4)
-        LinkedList shiftLinkedList5 = new LinkedList(5)
+        LinkedList linkedList0 = new LinkedList(0)
+        LinkedList linkedList1 = new LinkedList(1)
+        LinkedList linkedList2 = new LinkedList(2)
+        LinkedList linkedList3 = new LinkedList(3)
+        LinkedList linkedList4 = new LinkedList(4)
+        LinkedList linkedList5 = new LinkedList(5)
 
-        shiftLinkedList0.next = shiftLinkedList1
-        shiftLinkedList1.next = shiftLinkedList2
-        shiftLinkedList2.next = shiftLinkedList3
-        shiftLinkedList3.next = shiftLinkedList4
-        shiftLinkedList4.next = shiftLinkedList5
+        linkedList0.next = linkedList1
+        linkedList1.next = linkedList2
+        linkedList2.next = linkedList3
+        linkedList3.next = linkedList4
+        linkedList4.next = linkedList5
 
         when:
-        LinkedList result = shiftLinkedList.shiftLinkedList(shiftLinkedList0, 2)
+        LinkedList result = shiftLinkedList.shiftLinkedList(linkedList0, 2)
 
         then:
         result.value == 4
@@ -37,21 +37,21 @@ class LinkedListTest extends Specification {
         ShiftLinkedList shiftLinkedList = new ShiftLinkedList()
 
         given:
-        LinkedList shiftLinkedList0 = new LinkedList(0)
-        LinkedList shiftLinkedList1 = new LinkedList(1)
-        LinkedList shiftLinkedList2 = new LinkedList(2)
-        LinkedList shiftLinkedList3 = new LinkedList(3)
-        LinkedList shiftLinkedList4 = new LinkedList(4)
-        LinkedList shiftLinkedList5 = new LinkedList(5)
+        LinkedList linkedList0 = new LinkedList(0)
+        LinkedList linkedList1 = new LinkedList(1)
+        LinkedList linkedList2 = new LinkedList(2)
+        LinkedList linkedList3 = new LinkedList(3)
+        LinkedList linkedList4 = new LinkedList(4)
+        LinkedList linkedList5 = new LinkedList(5)
 
-        shiftLinkedList0.next = shiftLinkedList1
-        shiftLinkedList1.next = shiftLinkedList2
-        shiftLinkedList2.next = shiftLinkedList3
-        shiftLinkedList3.next = shiftLinkedList4
-        shiftLinkedList4.next = shiftLinkedList5
+        linkedList0.next = linkedList1
+        linkedList1.next = linkedList2
+        linkedList2.next = linkedList3
+        linkedList3.next = linkedList4
+        linkedList4.next = linkedList5
 
         when:
-        LinkedList result = shiftLinkedList.shiftLinkedList(shiftLinkedList0, -2)
+        LinkedList result = shiftLinkedList.shiftLinkedList(linkedList0, -2)
 
         then:
         result.value == 2
@@ -60,6 +60,36 @@ class LinkedListTest extends Specification {
         result.next.next.next.value == 5
         result.next.next.next.next.value == 0
         result.next.next.next.next.next.value == 1
+        result.next.next.next.next.next.next == null
+    }
+
+    def "algo expert failing test case 8"() {
+        ShiftLinkedList shiftLinkedList = new ShiftLinkedList()
+
+        given:
+        LinkedList linkedList0 = new LinkedList(0)
+        LinkedList linkedList1 = new LinkedList(1)
+        LinkedList linkedList2 = new LinkedList(2)
+        LinkedList linkedList3 = new LinkedList(3)
+        LinkedList linkedList4 = new LinkedList(4)
+        LinkedList linkedList5 = new LinkedList(5)
+
+        linkedList0.next = linkedList1
+        linkedList1.next = linkedList2
+        linkedList2.next = linkedList3
+        linkedList3.next = linkedList4
+        linkedList4.next = linkedList5
+
+        when:
+        LinkedList result = shiftLinkedList.shiftLinkedList(linkedList0, 8)
+
+        then:
+        result.value == 4
+        result.next.value == 5
+        result.next.next.value == 0
+        result.next.next.next.value == 1
+        result.next.next.next.next.value == 2
+        result.next.next.next.next.next.value == 3
         result.next.next.next.next.next.next == null
     }
 }
